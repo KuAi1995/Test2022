5ebf7609b82e1a679e10d58d71aa05499a1a8946
zhuyuanxue <1439534715@qq.com>
2022-04-15 00:53:18 +0800

    Android 11 包名可及性bug

--- AIDL/TwoAppAndNoAIDL/clientapplication/src/main/java/com/fulin/clientapplication/MainActivity.java
+++ AIDL/TwoAppAndNoAIDL/clientapplication/src/main/java/com/fulin/clientapplication/MainActivity.java
@@ -5,13 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate");
         Intent intent = new Intent();
         intent.setComponent(new ComponentName("com.fulin.serverapplication","com.fulin.serverapplication.BookService"));
         intent.putExtra("name", "book1");

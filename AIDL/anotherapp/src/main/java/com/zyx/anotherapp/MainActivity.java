886950f59a1f257050ebabc98fff85aec5a9cf86
zhuyuanxue <1439534715@qq.com>
2022-04-17 16:13:43 +0800

    通过aidl传输数据

--- AIDL/anotherapp/src/main/java/com/zyx/anotherapp/MainActivity.java
+++ AIDL/anotherapp/src/main/java/com/zyx/anotherapp/MainActivity.java
@@ -8,12 +8,22 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.os.RemoteException;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.widget.EditText;
+
+import com.zyx.startservicefromanotherapp.IAppServiceRemoteBinder;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, ServiceConnection {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
+
     private Intent serviceIntent;
+
+    private EditText etInput;
+
+    private IAppServiceRemoteBinder binder = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +33,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         serviceIntent = new Intent();
         serviceIntent.setComponent(new ComponentName("com.zyx.startservicefromanotherapp","com.zyx.startservicefromanotherapp.AppService"));
 
+        etInput = findViewById(R.id.etInput);
         findViewById(R.id.btnStartService).setOnClickListener(this);
         findViewById(R.id.btnStopService).setOnClickListener(this);
         findViewById(R.id.btnBindService).setOnClickListener(this);
         findViewById(R.id.btnUnbindService).setOnClickListener(this);
+        findViewById(R.id.btnSync).setOnClickListener(this);
     }
 
 
@@ -44,19 +56,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             case R.id.btnUnbindService:
                 unbindService(this);
+                binder = null;
                 break;
+            case R.id.btnSync:
+                if (binder != null){
+                    try {
+                        binder.setData(etInput.getText().toString());
+                    } catch (RemoteException e) {
+                        e.printStackTrace();
+                    }
+                }                break;
+
         }
     }
 
 
     @Override
     public void onServiceConnected(ComponentName name, IBinder service) {
-        System.out.println("Bind Service");
-        System.out.println(service);
+        binder = IAppServiceRemoteBinder.Stub.asInterface(service);
+        Log.d(TAG,"Bind Service");
     }
 
     @Override
     public void onServiceDisconnected(ComponentName name) {
-        System.out.println("unBind Service");
+        Log.d(TAG,"unBind Service");
     }
 }
--- AIDL/oneapp/src/main/java/com/zyx/startservicefromanotherapp/AppService.java
+++ AIDL/oneapp/src/main/java/com/zyx/startservicefromanotherapp/AppService.java
@@ -8,6 +8,11 @@ import android.util.Log;
 
 public class AppService extends Service {
     private static final String TAG = AppService.class.getSimpleName();
+
+    private String data = "默认数据";
+
+    private boolean running = false;
+
     public AppService() {
     }
 
@@ -19,22 +24,43 @@ public class AppService extends Service {
             public void basicTypes(int anInt, long aLong, boolean aBoolean, float aFloat, double aDouble, String aString) throws RemoteException {
 
             }
+
+            @Override
+            public void setData(String data) throws RemoteException {
+                AppService.this.data = data;
+            }
+
+
         };
     }
 
     @Override
     public void onCreate() {
         super.onCreate();
+        Log.d(TAG,"Service started");
 
-        System.out.println("Service started");
-        Log.d(TAG, "Service started");
+        new Thread(){
+            @Override
+            public void run() {
+                super.run();
+
+                Log.d(TAG,"data is : " + data );
+                running = true;
+                while (running){
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }.start();
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-
-        System.out.println("Service destroy");
-        Log.d(TAG, "Service destroy");
+        running = false;
+        Log.d(TAG,"Service destory");
     }
 }
